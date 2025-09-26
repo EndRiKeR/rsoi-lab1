f6c358da3711c081ba664bef6396ee4fbe1b1452
EndRiKeR <41268968+EndRiKeR@users.noreply.github.com>
2025-09-26 20:07:32 +0300

    fix db

--- Program.cs
+++ Program.cs
@@ -12,44 +12,43 @@ builder.Services.AddControllers();
 
 builder.Services.AddTransient<IRepository<Person>, PersonRepository>();
 
-// builder.Services.AddDbContext<Context>(options =>
-//     {
-//         options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-//         // options.EnableSensitiveDataLogging();
-//     },
-//     ServiceLifetime.Scoped,
-//     ServiceLifetime.Scoped);
+builder.Services.AddDbContext<Context>(options =>
+    {
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+        options.EnableSensitiveDataLogging();
+    },
+    ServiceLifetime.Scoped,
+    ServiceLifetime.Scoped);
 
 var app = builder.Build();
 
-// Ждем пока БД станет доступна
-await WaitForDb(builder.Configuration.GetConnectionString("DefaultConnection"));
-
-async Task WaitForDb(string connString)
-{
-    const int maxAttempts = 10;
-    var attempt = 0;
-    
-    while (attempt < maxAttempts)
-    {
-        try
-        {
-            using var connection = new NpgsqlConnection(connString);
-            await connection.OpenAsync();
-            Console.WriteLine("Database connected successfully!");
-            return;
-        }
-        catch (Exception ex)
-        {
-            attempt++;
-            Console.WriteLine($"Database connection attempt {attempt} failed: {ex.Message}");
-            await Task.Delay(2000); // Ждем 2 секунды
-        }
-    }
-    
-    throw new Exception("Could not connect to database after all attempts");
-}
-
+// await WaitForDb(builder.Configuration.GetConnectionString("DefaultConnection"));
+//
+// async Task WaitForDb(string connString)
+// {
+//     const int maxAttempts = 10;
+//     var attempt = 0;
+//     
+//     while (attempt < maxAttempts)
+//     {
+//         try
+//         {
+//             using var connection = new NpgsqlConnection(connString);
+//             await connection.OpenAsync();
+//             Console.WriteLine("Database connected successfully!");
+//             return;
+//         }
+//         catch (Exception ex)
+//         {
+//             attempt++;
+//             Console.WriteLine($"Database connection attempt {attempt} failed: {ex.Message}");
+//             await Task.Delay(2000); // Ждем 2 секунды
+//         }
+//     }
+//     
+//     throw new Exception("Could not connect to database after all attempts");
+// }
+//
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
