38e04e7a91fe273f28ffb049f04abe4c9fa9b334
EndRiKeR <41268968+EndRiKeR@users.noreply.github.com>
2025-09-26 22:32:04 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,15 +11,10 @@ builder.Services.AddControllers();
 
 builder.Services.AddTransient<IRepository<Person>, PersonRepository>();
 
-var dbHost = builder.Configuration["DB_HOST"] ?? "postgres";
-var dbPort = builder.Configuration["DB_PORT"] ?? "5432";
-var dbName = builder.Configuration["DB_NAME"];
-var dbUser = builder.Configuration["DB_USER"];
-var dbPassword = builder.Configuration["DB_PASSWORD"];
-
-var connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPassword};";
-
-builder.Services.AddDbContext<Context>(options => { options.UseNpgsql(connectionString); });
+builder.Services.AddDbContext<Context>(options =>
+    {
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+    });
 
 var app = builder.Build();
 
